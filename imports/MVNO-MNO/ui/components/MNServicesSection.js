0bb3a318671d6967e4150fcdc22ccaa5022bb67a
PandyaPreet <preetpandya456@gmail.com>
2025-10-15 15:27:37 +0530

    fix: some fix

--- imports/MVNO-MNO/ui/components/MNServicesSection.js
+++ imports/MVNO-MNO/ui/components/MNServicesSection.js
@@ -50,8 +50,12 @@ const MNServicesSection = () => {
       </TitleCardContainer>
       <ServicesCardContainer $direction="column">
         <ServicesCardWrapper $alignitems="center">
-          {RETAILER_SERVICES.map((card) => (
-            <ServicesCard $direction="column" bgColor={card.bgcolor}>
+          {RETAILER_SERVICES.map((card, index) => (
+            <ServicesCard
+              $direction="column"
+              bgcolor={card.bgcolor}
+              key={index}
+            >
               <Image height={72} width={72} src={card.icon} alt="some icon" />
               <ServiceCardDescription>{card.text}</ServiceCardDescription>
             </ServicesCard>
