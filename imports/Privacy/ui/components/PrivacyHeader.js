cb281aee562a3a6cec44f40f36bf807e1dd649ac
PandyaPreet <preetpandya456@gmail.com>
2025-10-16 15:27:22 +0530

    fix: some fix

--- imports/Privacy/ui/components/PrivacyHeader.js
+++ imports/Privacy/ui/components/PrivacyHeader.js
@@ -2,14 +2,16 @@
 
 import HeaderBrandLogo from "@/components/svg/HeaderBrandLogo";
 import Flex from "@/lib/atoms/Flex";
+import { useRouter } from "next/navigation";
 import React from "react";
 import styled from "styled-components";
 
 const PrivacyHeader = () => {
+  const router = useRouter();
   return (
     <HeaderContainer $justifycontent="space-between" $alignitems="center">
       <div />
-      <LogoContainer $direction="column">
+      <LogoContainer $direction="column" onClick={() => router.push("/")}>
         <HeaderBrandLogo />
       </LogoContainer>
     </HeaderContainer>
@@ -29,4 +31,5 @@ const LogoContainer = styled(Flex)`
   width: 90%;
   padding: 8px;
   gap: 10px;
+  cursor: pointer;
 `;
