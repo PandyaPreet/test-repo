c51eacd0f3f6306cad5384a032996943d4f1db1d
PandyaPreet <preetpandya456@gmail.com>
2025-10-17 17:11:18 +0530

    fix: updated robots

--- app/robots.js
+++ app/robots.js
@@ -1,3 +1,4 @@
+// app/robots.js
 export default function robots() {
   const siteUrl = "https://staging.ensureprotect.com";
 
@@ -8,7 +9,15 @@ export default function robots() {
         allow: "/",
         disallow: ["/api/"],
       },
+
+      { userAgent: "GPTBot", allow: "/" },
+      { userAgent: "Google-Extended", allow: "/" },
+      { userAgent: "ClaudeBot", allow: "/" },
+      { userAgent: "PerplexityBot", allow: "/" },
+      { userAgent: "Amazonbot", allow: "/" },
+      { userAgent: "CCBot", allow: "/" },
     ],
+
     sitemap: `${siteUrl}/sitemap.xml`,
     host: siteUrl,
   };
