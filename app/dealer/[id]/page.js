441a3fa4bab82aaea2970ffcd612289aed172a9c
Shubhh2903 <shubhamlilawala691@gmail.com>
2025-10-24 18:39:50 +0530

    feat: all dealer login

--- app/dealer/[id]/page.js
+++ app/dealer/[id]/page.js
@@ -1,60 +1,8 @@
-"use client";
-import { useParams } from "next/navigation";
+import DealersLoginPage from "@/imports/Dealers/DealersLoginPage";
 import React from "react";
 
-function page() {
-  const { id } = useParams();
-  const validDealers = [
-    "atwoods",
-    "5devices",
-    "ruralking",
-    "rides",
-    "bikemart",
-    "gorhambikeandski",
-    "landrys",
-    "pedal",
-    "wheelandsprocket",
-    "globalbikes",
-    "seeds",
-    "lmfleetsupply",
-    "fcfarmandhome",
-    "theisens",
-    "scw",
-    "buchheit",
-    "shoppers",
-    "instaprotek",
-    "fhsfpp-frame-brochure",
-    "fhs",
-    "education",
-    "biggear",
-    "eastwood",
-  ];
-  const iframeSrc = `https://ensureprotect.com/${id}/`;
-
-  return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-        margin: 0,
-        padding: 0,
-        overflow: "hidden",
-      }}
-    >
-      <iframe
-        src={iframeSrc}
-        style={{
-          border: "none",
-          height: "100%",
-          width: "100%",
-          margin: 0,
-          padding: 0,
-          display: "block",
-        }}
-        title={`Dealer - ${id}`}
-      />
-    </div>
-  );
-}
+const page = () => {
+  return <DealersLoginPage />;
+};
 
 export default page;
---
+++ imports/Dealers/DealersLoginPage.js
@@ -0,0 +1,60 @@
+"use client";
+import { useParams } from "next/navigation";
+import React from "react";
+
+function DealersLoginPage() {
+  const { id } = useParams();
+  const validDealers = [
+    "atwoods",
+    "5devices",
+    "ruralking",
+    "rides",
+    "bikemart",
+    "gorhambikeandski",
+    "landrys",
+    "pedal",
+    "wheelandsprocket",
+    "globalbikes",
+    "seeds",
+    "lmfleetsupply",
+    "fcfarmandhome",
+    "theisens",
+    "scw",
+    "buchheit",
+    "shoppers",
+    "instaprotek",
+    "fhsfpp-frame-brochure",
+    "fhs",
+    "education",
+    "biggear",
+    "eastwood",
+  ];
+  const iframeSrc = `https://ensureprotect.com/${id}/`;
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+      }}
+    >
+      <iframe
+        src={iframeSrc}
+        style={{
+          border: "none",
+          height: "100%",
+          width: "100%",
+          margin: 0,
+          padding: 0,
+          display: "block",
+        }}
+        title={`Dealer - ${id}`}
+      />
+    </div>
+  );
+}
+
+export default DealersLoginPage;
