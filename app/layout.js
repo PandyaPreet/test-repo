97813ba16e25bceedb936310fd6dba6b96ac32a5
jaril5976 <rajjariwala5976@gmail.com>
2025-10-28 12:26:22 +0530

    task: update google anayltics

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,9 @@
 import StyledComponentsRegistry from "@/lib/StyledComponentsRegistry";
 import ProviderLayout from "@/lib/ProviderLayout";
 import { unstable_ViewTransition as ViewTransition } from "react";
-import Script from "next/script";
-
-const SITE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL || "https://ensureprotect.com";
 
 export const metadata = {
-  metadataBase: new URL(SITE_URL),
+  metadataBase: new URL("https://ensureprotect.com"),
   robots: { index: true, follow: true },
   icons: { icon: "/favicon.svg" },
   alternates: {
@@ -24,18 +20,25 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-P38G9X9LF3"
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-P38G9X9LF3`}
           strategy="afterInteractive"
         />
-        <Script id="ga" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-P38G9X9LF3');
-          `}
-        </Script>
+        <script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-P38G9X9LF3', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body suppressHydrationWarning>
         <StyledComponentsRegistry>
